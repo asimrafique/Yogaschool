5ae6f47c8314f2bcfe19511ea8fa6d6d752b9690
asim <asimrafique11@yahoo.com>
2023-02-17 23:50:46 +0500

    redo the request file changes

--- app/Http/Requests/Student/RegistrationRequest.php
+++ app/Http/Requests/Student/RegistrationRequest.php
@@ -4,13 +4,15 @@ namespace App\Http\Requests\Student;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class OnlineRegistrationRequest extends FormRequest {
+class RegistrationRequest extends FormRequest
+{
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
-    public function authorize() {
+    public function authorize()
+    {
         return true;
     }
 
@@ -19,116 +21,30 @@ class OnlineRegistrationRequest extends FormRequest {
      *
      * @return array
      */
-    public function rules() {
-
-
-        if (request()->get('section_no')) {
-            $favcolor=request()->get('section_no');
-
-            switch ($favcolor) {
-                case 1:
-                    return ['course_id'                       => 'required'];
-
-                    break;
-                case 2:
-
-                    $passwordRule='required';
-                    $emailRule='required';
-                    if (request()->get('check')==false) {
-                        $emailRule='required|email|unique:users,email|max:191';
-                        // $phoneRule='required_without_all:email|unique:users,phone|max:191';
-                        $passwordRule='required|min:8|max:191|confirmed';
-                    }
-                    else
-                    {
-                        $emailRule='required|exists:users,email';
-                    }
-                    return [  'email'                         => $emailRule,
-                        'password'                         => $passwordRule];
-
-
-                    break;
-                case 3:
-
-
-                    if (request()->get('check')==false) {
-                        return [
-                            'first_name'                      => 'required|min:2',
-                            'date_of_birth'                   => 'required|date',
-                            'gender'                          => 'required',
-
-                        ];
-                    }
-
-                    break;
-                case 4:
-
-
-
-                    return [
-                        'contact_number'                  => 'required',
-                        'address_line_1'                  => 'required',
-
-                        'city'                            => 'required',
-                        'state'                           => 'required',
-                        'zipcode'                         => 'required',
-                        'country'                         => 'required',
-
-                    ];
-
-
-                    break;
-                case 5:
-
-
-
-                    return [
-
-                    ];
-
-
-                    break;
-                default:
-                    echo "Your favorite color is neither red, blue, nor green!";
-            }
-
-        }
-
+    public function rules()
+    {
         $relations = implode(',', gv(getVar('list'), 'relations', []));
-        $passwordRule='required';
-        $emailRule='required';
-        if (request()->get('check')==false) {
-            $emailRule='required|email|unique:users,email|max:191';
-            // $phoneRule='required_without_all:email|unique:users,phone|max:191';
-            $passwordRule='required|min:8|max:191|confirmed';
-        }
-        else
-        {
-            $emailRule='required|exists:users,email';
-        }
 
-        return [
-            'first_name'                      => 'required|min:2',
-            // 'last_name'                       => 'required|min:2',
-            'date_of_birth'                   => 'required|date',
-            'contact_number'                  => 'required',
-            'gender'                          => 'required',
-            'first_guardian_name'             => 'required',
-            'first_guardian_relation'         => 'required|different:second_guardian_relation|in:'.$relations,
-            'second_guardian_name'            => 'required',
-            'second_guardian_relation'        => 'required|different:first_guardian_relation|in:'.$relations,
-            'first_guardian_email'            => 'email',
-            'first_guardian_contact_number_1' => 'required',
-            'course_id'                       => 'required',
-            'address_line_1'                  => 'required',
-            'city'                            => 'required',
-            'state'                           => 'required',
-            'zipcode'                         => 'required',
-            'country'                         => 'required',
-            'email'                         => $emailRule,
-            'password'                         => $passwordRule,
-
-        ];
+        if (request('student_type') == 'new') {
+            return [
+                'first_name'                      => 'required|min:2',
+                'date_of_birth'                   => 'required|date',
+                'date_of_registration'            => 'required|date|after:date_of_birth',
+                'contact_number'                  => 'required',
+                'gender'                          => 'required',
+                'first_guardian_name'             => 'required_if:parent_type,new|min:3',
+                'first_guardian_relation'         => 'required_if:parent_type,new|different:second_guardian_relation|in:'.$relations,
+                'second_guardian_relation' => 'required_with:second_guardian_name|different:first_guardian_relation|in:'.$relations,
+                'first_guardian_contact_number_1' => 'required_if:parent_type,new',
+                'course_id'                       => 'required'
+            ];
+        } else {
+            return [
+                'student_id'              => 'required',
+                'date_of_registration'    => 'required|date|after:date_of_birth',
+                'course_id'               => 'required'
+            ];
+        }
     }
 
     /**
@@ -136,23 +52,21 @@ class OnlineRegistrationRequest extends FormRequest {
      *
      * @return array
      */
-    public function attributes() {
+    public function attributes()
+    {
         return [
-            'first_name'                      => trans('student.first_name'),
-            'last_name'                       => trans('student.last_name'),
-            'date_of_birth'                   => trans('student.date_of_birth'),
-            'contact_number'                  => trans('student.contact_number'),
-
-            'first_guardian_name'             => trans('student.first_guardian_name'),
-            'second_guardian_name'            => trans('student.second_guardian_name'),
-            'first_guardian_email'            => trans('student.first_guardian_email'),
-            'first_guardian_contact_number_1' => trans('student.first_guardian_contact_number'),
-            'course_id'                       => trans('academic.course'),
-            'address_line_1'                  => trans('student.address_line_1'),
-            'city'                            => trans('student.city'),
-            'state'                           => trans('student.state'),
-            'zipcode'                         => trans('student.zipcode'),
-            'country'                         => trans('student.country'),
+            'student_id'               => trans('student.student'),
+            'first_name'               => trans('student.first_name'),
+            'last_name'                => trans('student.last_name'),
+            'date_of_birth'            => trans('student.date_of_birth'),
+            'date_of_registration'     => trans('student.date_of_registration'),
+            'contact_number'           => trans('student.contact_number'),
+            'gender'                   => trans('student.gender'),
+            'first_guardian_name'      => trans('student.first_guardian_name'),
+            'first_guardian_relation'  => trans('general.relation'),
+            'second_guardian_name'     => trans('student.second_guardian_name'),
+            'second_guardian_relation' => trans('general.relation'),
+            'course_id'                => trans('academic.course')
         ];
     }
 
@@ -164,8 +78,11 @@ class OnlineRegistrationRequest extends FormRequest {
     public function messages()
     {
         return [
+            'first_guardian_name.required_if'             => trans('validation.required', ['attribute' => trans('student.first_guardian_name')]),
+            'first_guardian_contact_number_1.required_if' => trans('validation.required', ['attribute' => trans('student.first_guardian_contact_number_1')]),
             'first_guardian_relation.different' => trans('general.different_custom', ['attribute' => trans('general.relation')]),
-            'second_guardian_relation.different' => trans('general.different_custom', ['attribute' => trans('general.relation')])
+            'second_guardian_relation.different' => trans('general.different_custom', ['attribute' => trans('general.relation')]),
+            'second_guardian_relation.required_with' => trans('validation.required', ['attribute' => trans('general.relation')])
         ];
     }
 }
